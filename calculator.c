d1095dc5f4604dd023862fb1bb75229e2eb7d9a7
LordOfDeadbush <37131666+LordOfDeadbush@users.noreply.github.com>
2023-03-06 17:44:23 -0800

    beginning 4 function calculator project

--- calculator.c
+++ calculator.c
@@ -1,7 +1,37 @@
+/*
+
+template calculator: https://www.amazon.com/Texas-Instruments-TI-108-Elementary/dp/B001F0271O
+
+4 Function Calculator:
+
+! number input
+Has Number inputs: 
+0123456789
+
+Has decimal and negative numbers
+
+! operations
+Has 4 basic operations
++ - * /
+
+has sqrt
+
+has % (divide by 100)
+
+has = (display result)
+
+! other operations
+Has memory management (M) <- do this later
+
+* NOTE: this will not follow PEMDAS rules
+*/
+
+
 #include <stdio.h>
 int main() {
-    int name[100];
-    printf("CALCULATOR\n\nEnter your name:");
-    scanf("%[^\n]%*c", &name);
-    printf("Hello, %s", name);
+//     int name[100];
+//     printf("CALCULATOR\n\nEnter your name:");
+//     scanf("%[^\n]%*c", &name);
+//     printf("Hello, %n", name);
+    
 }
