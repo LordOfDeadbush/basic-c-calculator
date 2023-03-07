960d38c01490acac9c284ebfab5552a172b3207b
LordOfDeadbush <37131666+LordOfDeadbush@users.noreply.github.com>
2023-03-06 18:21:15 -0800

    got essential functions working
    number input and print result both work now

--- calculator.c
+++ calculator.c
@@ -23,15 +23,66 @@ has = (display result)
 ! other operations
 Has memory management (M) <- do this later
 
-* NOTE: this will not follow PEMDAS rules
+
+
+? NOTE: this will not follow PEMDAS rules
 */
 
-
 #include <stdio.h>
+
+// * inputs
+float number_input() { // & works
+    float input;
+    printf(">");
+    scanf("%f", &input);
+    return input;
+}
+
+float func_input() {
+    return 0;
+}
+
+// * basic operations
+float add(float x) {
+    float y = number_input();
+    return x + y;
+}
+
+float sub(float x) {
+    float y = number_input();
+    return x - y;
+}
+
+float mult(float x) {
+    float y = number_input();
+    return x * y;
+}
+
+float div(float x) {
+    float y = number_input();
+    return x / y;
+}
+
+// * other operations
+// ? these operations only take the x parameter, so y is not required
+
+float root(float x) {
+    return x;
+}
+
+float perc(float x) {
+    return x / 100;
+}
+
+float print_result(float x) { // & works
+    printf("=%g",x);
+    return x;
+}
+
 int main() {
 //     int name[100];
 //     printf("CALCULATOR\n\nEnter your name:");
 //     scanf("%[^\n]%*c", &name);
 //     printf("Hello, %n", name);
-    
+    print_result(number_input());
 }
